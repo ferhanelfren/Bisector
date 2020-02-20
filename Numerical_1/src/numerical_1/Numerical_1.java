074fca2f0dac45a4f7b44e1c82cb79e0c10d5de5
ferhanelfren <60415663+ferhanelfren@users.noreply.github.com>
2020-02-19 21:11:31 -0800

    Add files via upload

---
+++ Numerical_1/src/numerical_1/Numerical_1.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numerical_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author NM-Mao
+ */
+public class Numerical_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+       
+        
+        
+     //c=a+b/2
+        
+        
+        //a,b,c,f(a),f(b),f(c)
+        Scanner scn=new Scanner(System.in);
+        
+     //int a=1,b=2;
+       int a=1,b=2,d,e,f,g;
+       double c=1.5;
+       int arr[];
+     
+     System.out.print( "a= "+a);    System.out.print("         b= "+b +""); System.out.print("         c=" +c );
+    
+   
+    // System.out.print("c="+c +"");
+    //  c=a+b/2;
+     
+                     
+     
+        d= (int) Math.pow(a, 2); 
+     
+            e= d - 3;
+       
+            
+            
+        System.out.println("         f(a)="+e);
+     
+     
+       
+     
+     
+     
+     
+     
+      
+       
+       
+      
+        
+        
+        
+        
+     
+  
+       
+         
+    }
+        
+        
+       void dasd()
+       
+       
+       
+       {
+           
+           
+       }
+    
+    
+    
+    
+        
+        
+        
+        
+    }
+    
+
---
+++ Numerical_1/src/numerical_1/v.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numerical_1;
+
+/**
+ *
+ * @author NM-Mao
+ */
+class v {
+
+    static void add(String[] column) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
